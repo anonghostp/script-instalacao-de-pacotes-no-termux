bf3623ab1012b2005c9617c7827e8f92fbfd354f
hacker blackhat <lulzsec.india1989@gmail.com>
2024-03-07 15:37:08 +0000

    script para instalacao de pacotes no termux, aoaguem um sh pois vai com 2 sh

---
+++ install.sh.sh
@@ -0,0 +1,68 @@
+#!/data/data/com.termux/files/usr/bash
+echo $grean
+echo ``------------------------------------------------------``
+echo `` SCRIPT BY ANONGHOST PORTUGAL ``
+echo ``------------------------------------------------------``
+
+echo
+
+echo ``######################################################``
+echo `` Script para instalacao de pacotes no termux ``
+echo ``######################################################``
+
+echo 
+echo
+echo
+echo
+
+echo","
+
+apt update        #update em andamento
+apt install python -y   #instalacao......
+apt install python2 -y
+apt install git -y
+apt install curl -y 
+apt install wget -y
+apt install dnsutils -y 
+apt install net-tools -y
+apt install build-essential -y
+apt install clang -y
+apt install rust -y
+apt install python-lxml -y
+apt install neofetch -y
+apt install figlet -y
+apt install ruby -y
+apt install golang -y
+apt install bash -y
+apt install openssl -y
+apt install openssl-tool -y
+apt install openssh -y
+apt install php -y
+apt install apache2 -y
+apt install dirb -y
+pkg install python-numpy -y
+pkg install electrum -y
+pkg install opencv-python -y
+pkg install asciinema -y
+pkg install matplotlib -y 
+pkg install python-cryptography -y 
+pkg install dialog -y
+pkg install pkg-config -y 
+pip install wheel
+pip install cython 
+pip install requests 
+pip2 install cython 
+pip2 install wheel 
+pip2 install requests 
+pip install bs4 
+pip install future 
+pip install lolcat
+
+echo "########################################"
+echo " DESFRUTEM DO SCRIPT "
+echo "########################################"
+
+echo " Instalacao terminada by by "
+echo " Espero que tenham gostado "
+
+exit
